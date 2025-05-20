ff2eab4d2b2ca9efcf0e805d969846cd661262b2
Tanish <134039330+Tanistoic@users.noreply.github.com>
2025-05-20 12:18:15 +0530

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,135 @@
+-- 1. What are the top-paying data analyst jobs in India?
+
+SELECT
+    job_id,
+    job_title,
+    name as company_name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_country = 'India' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
+
+
+-- 2. What skills are required for these top-paying jobs?
+
+with top_paying_jobs as (
+    SELECT
+        job_id,
+        job_title,
+        name as company_name,
+        salary_year_avg
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_country = 'India' AND
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    top_paying_jobs.*,
+    skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC;
+
+
+-- 3. What skills are most in demand for data analysts?
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND job_country = 'India' 
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5;
+
+
+-- 4. Which skills are associated with higher salaries?
+
+SELECT 
+    skills,
+    ROUND(AVG(salary_year_avg), 0) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND job_country = 'India'
+    AND salary_year_avg IS NOT NULL
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 15;
+
+
+-- 5. What are the most optimal skills to learn?
+
+WITH average_salary AS (
+    SELECT 
+        skills_job_dim.skill_id,
+        ROUND(AVG(salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst' 
+        AND job_country = 'India'
+        AND salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_job_dim.skill_id
+), skills_demand AS (
+    SELECT 
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst' 
+        AND job_country = 'India'
+        AND salary_year_avg IS NOT NULL 
+    GROUP BY
+        skills_dim.skill_id
+)
+
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM
+    skills_demand
+INNER JOIN  average_salary ON skills_demand.skill_id = average_salary.skill_id
+ORDER BY demand_count DESC,
+avg_salary DESC
+LIMIT 15
+
+
+
+
